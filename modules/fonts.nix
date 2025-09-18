e840c54b6abac6864feda7595344970f7519e53c
Niccolo' Zanotti <contact@niccolozanotti.com>
2025-09-18 10:56:23 +0200

    feat(fonts): add fonts required as deps for typst cv
    Ref: https://github.com/yunanwg/brilliant-CV

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -3,5 +3,9 @@
     pkgs.nerd-fonts.fira-code
     pkgs.nerd-fonts.jetbrains-mono
     pkgs.nerd-fonts.meslo-lg
+    pkgs.roboto
+    pkgs.font-awesome
+    pkgs.source-sans
+    pkgs.source-sans-pro
   ];
 }
