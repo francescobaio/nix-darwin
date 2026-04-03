d5a83c689db67ed31ba4ccbe12d76e8383c9be6b
Niccolo' Zanotti <contact@niccolozanotti.com>
2026-04-03 12:07:45 +0200

    feat: update flake (unstable channel)
    * update flake from unstable channel as of 2026-04-02
    * in symlinks generation, pass "/Applications" str as list to deal with
      breaking change in underlying Perl build script pkgs.buildEnv

--- modules/system.nix
+++ modules/system.nix
@@ -20,7 +20,7 @@
     env = pkgs.buildEnv {
       name = "system-applications";
       paths = config.environment.systemPackages;
-      pathsToLink = "/Applications";
+      pathsToLink = [ "/Applications" ];
     };
   in
     # alias nix-installed apps to /Applications (for indexing, etc ...)
